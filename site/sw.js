7c302632f0a8c02fabda961c00b0766a329e04af
dejavuGaming <168145350+Lowzzzz@users.noreply.github.com>
2026-09-17 09:22:21 -0400

    Rotate cache for iOS 0.3.3 clean dock UI

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const VERSION='loky-pc4-mobile-ios-0.3.2';
+const VERSION='loky-pc4-mobile-ios-0.3.3';
 const CACHE=`${VERSION}-shell`;
 const SHELL=['./','./index.html','./mobile.css','./sphere-mobile.js','./app.js','./live-mobile.js','./ios-audio-stability.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-512.png'];
 
